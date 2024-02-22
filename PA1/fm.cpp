9bf2bf71aebe8f8033974f6c8a6914ad0818127c
Aditya Das Sarma <143854419+adassarma@users.noreply.github.com>
2024-02-22 00:56:44 -0600

    Penultimate push

--- PA1/fm.cpp
+++ PA1/fm.cpp
@@ -520,6 +520,6 @@ int main(int argc,char **argv)
         }
         auto end_time = chrono::high_resolution_clock::now();
         cout<<"Runtime: "<<chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count() << "\n";
-        cout<<"Cutsize: "<<bestcutsize;
+        cout<<"Cutsize: "<<bestcutsize<<"\n";
         writetoFile(argv[2]);
 }
